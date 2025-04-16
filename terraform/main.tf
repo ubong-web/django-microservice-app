cb4fd0861e6160129095cf00d668e3698df00077
ubong-web <ubongj368@gmail.com>
2025-04-16 21:46:01 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ terraform {
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ubuntu
-              curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-\$(uname -s)-\$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
               EOF
   tags = {
